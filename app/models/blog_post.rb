e4509c40406cb522ffe1ca157b66ff8a4d6c23f1
Clément Riquet <clementriq@gmail.com>
2025-02-06 12:06:00 +0100

    adding draft

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,4 +1,18 @@
 class BlogPost < ApplicationRecord
     validates :title, presence: true
     validates :body, presence: true
+
+    scope :draft,n -> { where(published_at: nil) }
+    scope :published, -> { where("published_at <= ?", Time.current) }
+    scope :scheduled, -> { where("published_at > ?", Time.current) }
 end
+
+BlogPost.all
+BlogPost.draft
+BlogPost.published
+BlogPost.scheduledx@
+
+`published_at` datetime field
+- nil
+- 1.year.ago
+- 1.year.from_now
---
+++ db/migrate/20250206104914_add_published_at_blog_posts.rb
@@ -0,0 +1,4 @@
+class AddPublishedAtBlogPosts < ActiveRecord::Migration[8.0]
+  def change
+  end
+end
